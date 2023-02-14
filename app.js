ebddcbd6eb4caf54b045948ec6687523a8a31358
edriso <edriso.dev@gmail.com>
2023-02-14 16:37:53 +0200

    update starter files

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 
-const express = require('express');
 const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.use(notFoundMiddleware);
@@ -18,9 +18,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
