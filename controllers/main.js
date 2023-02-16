a15cc9894882be623f811bcd94d97fd8c543f4e0
edriso <edriso.dev@gmail.com>
2023-02-16 22:32:27 +0200

    send token when login

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -6,7 +7,19 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError("Please provide email and password", 400);
   }
-  res.send("Fake Login/Signup Route");
+
+  // usually comes form DB
+  const imaginaryUserId = new Date().valueOf();
+
+  // *try to keep payload small, better experience for user (faster)
+  // **jwtSecret here is just for demo, in production use long, complex, and unguessable string value!
+  const token = jwt.sign(
+    { imaginaryUserId, username },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
+  );
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
